77248530f437494a746c8151041d88450c7b4fe1
Grant Lewison <60186503+glew2@users.noreply.github.com>
2022-12-15 23:47:48 -0500

    Rapid Protoype all changes

--- app.js
+++ app.js
@@ -1,45 +1,110 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const path = require("path");
+const app = express();
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const expressSession = require("express-session");
+const passport = require("passport");
+const Auth0Strategy = require("passport-auth0");
 
-var dataRouter = require('./routes/sample_data');
+require('dotenv').config(); 
 
-var app = express();
+const authRouter = require("./auth");
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+const { auth, requiresAuth } = require('express-openid-connect');
+app.use(
+  auth({
+    authRequired: false,
+    auth0Logout: true,
+    issuerBaseURL: process.env.ISSUER_BASE_URL,
+    baseURL: process.env.BASE_URL,
+    clientID: process.env.CLIENT_ID,
+    secret: process.env.SECRET,
+  })
+);
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+const session = {
+    secret: process.env.SESSION_SECRET,
+    cookie: {},
+    resave: false,
+    saveUnitialized: false
+}
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+if (app.get("env") === "production") {
+    session.cookie.secure = true;
+}
 
-app.use('/data', dataRouter);
+const strategy = new Auth0Strategy(
+    {
+      domain: process.env.AUTH0_DOMAIN,
+      clientID: process.env.AUTH0_CLIENT_ID,
+      clientSecret: process.env.AUTH0_CLIENT_SECRET,
+      callbackURL: process.env.AUTH0_CALLBACK_URL
+    },
+    function(accessToken, refreshToken, extraParams, profile, done) {
+      return done(null, profile);
+    }
+);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use(expressSession(session));
+
+passport.use(strategy);
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser((user, done) => {
+    done(null, user);
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+passport.deserializeUser((user, done) => {
+    done(null, user);
 });
 
-module.exports = app;
+app.use((req, res, next) => {
+    res.locals.isAuthenticated = req.isAuthenticated();
+    next();
+});
+  
+app.use("/", authRouter);
+app.get('/', function (req, res) {
+    res.render('index', {});
+});
+app.get('/workflow', function (req, res) {
+  res.render('workflow', {});
+});
+
+// app.get('/', (req, res) => {
+//     // res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
+//     res.sendFile(req.oidc.isAuthenticated() ? __dirname + '/data.html' : __dirname + '/index.html');
+// })
+
+// function login() {s
+//     alert("Hello");
+//     app.get('/', (req, res) => {
+//         alert("Hello");
+//         res.sendFile(__dirname + "login");
+//     })
+// }
+
+// app.get('/logout', (req, res) => {
+//     res.sendFile("index.html");
+// })
+
+// app.get('/profile', requiresAuth(), (req, res) => {
+//     res.send(JSON.stringify(req.oidc.user))
+// })
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`listening on port ${port}`);
+});
+
+// var myButton = document.getElementById('button');
+// myButton.onclick = function() {
+//     alert("hurb");
+//     res.sendFile(__dirname + "/login");
+//     window.location.href="http://localhost:3000/login"; 
+// }
